dafc48d795341ca8b30470725dd3f229da33993f
aniruddha patel <aniruddhapatel2000@gmail.com>
2023-04-09 18:42:13 +0530

    original commit

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,93 @@
+   const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  //thows an erroe when user tries to login while not filling all the information
+  if (!name ||!email||!password) {
+    res.status(400);
+    throw new Error("Plase Enter all the Feilds");
+  }
+
+  //findOne is a mongoDB query used to find whether the email exists in the data base or not
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Plase Enter all the Feilds");
+  }
+
+  //User.create is also an mongo db command that is used to insert or store the data the structure defined below
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create a user , try again later");
+  }
+});
+
+const authUser= asyncHandler(async(req,res)=>{
+
+   const{email,password}=req.body;
+
+   const user= await User.findOne({email});
+
+   if(user&&(await user.matchPassword(password)))
+   {
+      res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+      });
+   
+   }
+   else{
+      res.status(401);
+      throw new Error("Invalid Email or Password");
+
+   }
+
+})
+
+const allUsers=asyncHandler(async(req,res)=>{
+
+     const keyword=req.query.search?{
+ 
+         $or:[
+
+            {name:{$regex:req.query.search,$options:"i"}},
+            {email:{$regex:req.query.search,$options:"i"}},
+
+         ]        
+
+
+     }:{};
+
+    //  console.log(keyword);
+ 
+    const users= await  User.find(keyword).find({_id:{$ne:req.user._id}});
+    res.send(users);
+
+
+
+})
+
+
+module.exports = { registerUser,authUser,allUsers};
+
---
+++ backend/server.js
@@ -0,0 +1,48 @@
+//importing express
+const express = require("express");
+//importing chats
+const { chats } = require("./data/data");
+//importing an env file
+const dotenv = require("dotenv");
+//connecting database
+const connectDB = require("./config/db");
+const colors=require("colors")
+const userRoutes=require('./routes/userRoutes')
+const {notFound,errorHandler}=require("./middleware/errorMiddleware")
+
+//creating an instance for express
+const app = express();
+dotenv.config();
+
+connectDB();
+
+app.use(express.json()); //to accept json data 
+
+//creating an api
+app.get("/", (req, res) => {
+  res.send("Api is running Succesfullly");
+});
+
+//creating an complex API
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+// //creating an another end point with the id's of data
+
+// app.get("/api/chat/:id", (req, res) => {
+//   //   console.log(req.params.id);
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(singleChat);
+// });
+
+
+app.use('/api/user',userRoutes);
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log("server has beeen started on port ${PORT}".yellow.bold));
